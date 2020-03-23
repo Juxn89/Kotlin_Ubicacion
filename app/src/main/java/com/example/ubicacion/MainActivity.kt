4dbafda6542d9e4ce86d78ac433de64172c4aa37
Juan Gómez <gb.jc@outlokk.com>
2020-03-23 14:55:15 -0600

    Se implementa la funcionalidad de validar permisos y se agrega la referencia de Google Play Services

--- app/src/main/java/com/example/ubicacion/MainActivity.kt
+++ app/src/main/java/com/example/ubicacion/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.example.ubicacion
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity() {
+
+    private val permisoFineLocation = android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val permisoCoarseLocation = android.Manifest.permission.ACCESS_COARSE_LOCATION
+
+    private val CODIGO_SOLICITUD_PERMISO = 100
+
+    var fusedLocationClient:FusedLocationProviderClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = FusedLocationProviderClient(this)
+    }
+
+    private fun validarPersimisosUbicacion():Boolean {
+        val hayUbicacionPremisa = ActivityCompat.checkSelfPermission(this, permisoFineLocation) == PackageManager.PERMISSION_GRANTED
+        val hayUbicacionOrdinaria = ActivityCompat.checkSelfPermission(this, permisoCoarseLocation) == PackageManager.PERMISSION_GRANTED
+
+        return hayUbicacionPremisa && hayUbicacionOrdinaria
+    }
+
+    private fun obtenerUbicacion() {
+
+    }
+
+    private  fun pedirPermisos() {
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (validarPersimisosUbicacion()) {
+            obtenerUbicacion()
+        }
+        else {
+            pedirPermisos()
+        }
     }
 }
