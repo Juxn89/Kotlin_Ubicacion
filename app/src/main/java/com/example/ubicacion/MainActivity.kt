812f5f977acb24a6477c8dd8513872edf1bfc000
Juan Gómez <gb.jc@outlokk.com>
2020-03-23 15:44:11 -0600

    Se implementa la funcionalidad de última ubicación

--- app/src/main/java/com/example/ubicacion/MainActivity.kt
+++ app/src/main/java/com/example/ubicacion/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.ubicacion
 
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.tasks.OnSuccessListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,12 +33,44 @@ class MainActivity : AppCompatActivity() {
         return hayUbicacionPremisa && hayUbicacionOrdinaria
     }
 
+    @SuppressLint("MissingPermission")
     private fun obtenerUbicacion() {
+        fusedLocationClient?.lastLocation?.addOnSuccessListener(this, object: OnSuccessListener<Location>{
+            override fun onSuccess(p0: Location?) {
+                if (p0 != null){
+                    Toast.makeText(applicationContext, "${p0?.latitude} - ${p0?.longitude}", Toast.LENGTH_SHORT).show()
+                }
+            }
 
+        })
     }
 
     private  fun pedirPermisos() {
+        val deboProveerContexto = ActivityCompat.shouldShowRequestPermissionRationale(this, permisoFineLocation)
 
+        if (deboProveerContexto) {
+            solicitudPermiso()
+        } else {
+          solicitudPermiso()
+        }
+    }
+
+    private fun solicitudPermiso() {
+        requestPermissions(arrayOf(permisoFineLocation, permisoCoarseLocation), CODIGO_SOLICITUD_PERMISO)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            CODIGO_SOLICITUD_PERMISO -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(this, "No diste permiso para acceder a la ubicación", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onStart() {
